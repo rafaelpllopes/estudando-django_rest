1c9711696202d90b9f473b6a0eda48c26f84edd7
rafaelpllopes <rafael.pllopes@gmail.com>
2021-09-28 21:53:23 -0300

    Teste,  segurança lapidaçoes

---
+++ drf_lapidacoes-projeto/setup/urls.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from escola.views import (
+    AlunosViewSet,
+    CursosViewSet,
+    ListaAlunosMatriculados,
+    ListaMatriculasAluno,
+    MatriculaViewSet,
+)
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register("alunos", AlunosViewSet, basename="Alunos")
+router.register("cursos", CursosViewSet, basename="Cursos")
+router.register("matriculas", MatriculaViewSet, basename="Matriculas")
+
+urlpatterns = [
+    path("admin/", include("admin_honeypot.urls", namespace="admin_honeypot")),
+    path("controle_geral/", admin.site.urls),
+    path("", include(router.urls)),
+    path("alunos/<int:pk>/matriculas/", ListaMatriculasAluno.as_view()),
+    path("cursos/<int:pk>/matriculas/", ListaAlunosMatriculados.as_view()),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
